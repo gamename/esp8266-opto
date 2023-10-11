3a7da4ef0589da0b0a573ef07c654631a90cd7db
gamename <tennis.n.smith@gmail.com>
2023-10-11 06:46:59 -0500

    shortened description

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
-This is a simple script to turn on a WS2812 LED strip based on the state of
-a slotted opto-coupler mounted on a door.
+A simple script to turn on/off a WS2812 LED strip based on the state of a slotted opto-coupler
+mounted on a door.
 
 Here is the specific model:
-https://www.amazon.com/dp/B08977QFK5?psc=1&ref=ppx_yo2ov_dt_b_product_details
+    https://www.amazon.com/dp/B08977QFK5?psc=1&ref=ppx_yo2ov_dt_b_product_details
 
 Truth table:
     State           Opto-Coupler Value              Boolean Value
