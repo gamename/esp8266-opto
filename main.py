51f973019b65d7972f52188fce704ebacdc5be81
gamename <tennis.n.smith@gmail.com>
2023-10-11 06:43:53 -0500

    update doc to explain states/values

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from machine import Pin
+"""
+This is a simple script to turn on a WS2812 LED strip based on the state of
+a slotted opto-coupler.
+
+Here is the specific model:
+https://www.amazon.com/dp/B08977QFK5?psc=1&ref=ppx_yo2ov_dt_b_product_details
+
+Truth table:
+    State           Opto-Coupler Value              Boolean Value
+    ---------       --------------------------      -------------
+    Door Open       Unblocked (light detected)      True
+    Door Closed     Blocked (no light detected)     False
+"""
 import neopixel
+from machine import Pin
 
 # Pin configuration
 optocoupler_pin = Pin(4, Pin.IN, Pin.PULL_UP)  # GPIO4 (D2) for the optocoupler
@@ -17,10 +30,20 @@ ON = (255, 255, 255)
 
 # Function to control the LED strip based on optocoupler state
 def control_led_strip(optocoupler_state):
-    if optocoupler_state:  # Door open - Light detected on OptoCoupler
+    """
+    Take the state value of an Opto-Coupler and set LED strip values accordingly
+
+    :param optocoupler_state: Either True or False
+    :type optocoupler_state: bool
+    :return: Nothing
+    :rtype: None
+    """
+    if optocoupler_state:
+        # Door open - Unblocked, light detected
         led_strip.fill(ON)
     else:
-        led_strip.fill(OFF)  # Door closed - No light detected on OptoCoupler
+        # Door closed - Blocked, no light detected
+        led_strip.fill(OFF)
 
     # Update the LED strip
     led_strip.write()
